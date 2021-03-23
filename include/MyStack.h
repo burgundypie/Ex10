8942641d0a0a7b45a5ce3b80f944462dc4895d0a
burgundypie <kotyonok2001@mai.ru>
2021-03-23 20:11:37 +0300

    Симонова Арина

--- include/MyStack.h
+++
@@ -1,9 +0,0 @@
-// Copyright 2020 A.SHT
-#ifndef INCLUDE_MYSTACK_H_
-#define INCLUDE_MYSTACK_H_
-
-template<class T>
-class MyStack {
-};
-
-#endif  // INCLUDE_MYSTACK_H_
--- src/postfix.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright 2020 A.SHT
-#include "MyStack.h"
-#include "postfix.h"
-
-std::string infix2postfix(std::string infix) {
-  return infix;
-}
